44e66e89200e0b99c5460e93ce3206096e2b0de1
Benjamin Schmid <bene.schmid@gmail.com>
2023-11-16 16:03:15 +0100

    Add the microscope example application.
    Sync with 8f92720e0f940b446750fa686d5e31cac3b2a294

---
+++ nls/microscope/interpolator.py
@@ -0,0 +1,35 @@
+from typing import Callable
+
+Getter = Callable[[], float]
+Setter = Callable[[int, float], None]
+
+
+class Interpolator:
+    def __init__(self, getter: Getter, setter: Setter, vTo: float, nCycles: int):
+        self._getter = getter
+        self._setter = setter
+        self._vTo = vTo
+        self._nCycles = nCycles
+        self._vFrom: float = 0
+
+    def _initialize(self):
+        self._vFrom = self._getter()
+
+    def interpolate(self, cycle: int) -> None:
+        if cycle == 0:
+            self._initialize()
+        interpolated: float = self._vFrom + (cycle + 1) * (self._vTo - self._vFrom) / self._nCycles
+        self._setter(cycle, interpolated)
+
+
+def main():
+    nCycles: int = 10
+    interpolator: Interpolator = Interpolator(
+        lambda: 5, lambda c, v: print(str(c) + ": " + str(v)), vTo=15, nCycles=nCycles)
+
+    for i in range(nCycles):
+        interpolator.interpolate(i)
+
+
+if __name__ == "__main__":
+    main()
